525e57fe3c97b23829aa8e95fbe3ef4dd69a3a3f
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2025-03-14 10:12:56 -0300

    Comecei a criação dos templates

--- base/views.py
+++ base/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
+
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Olá, mundo!")
+    tasks = Task.objects.all()
+    tasks_count = tasks.count()
+    uncompleted_tasks = tasks.filter(completed=False).count()
+    
+    context = {'tasks': tasks, 'tasks_count': tasks_count, 'uncompleted_tasks': uncompleted_tasks}
+        
+    return render(request, 'base/index.html', context)
+
+
+def task(request, pk):   
+    task = Task.objects.get(id=pk)
+
+    context = {'task': task}    
+    
+    return render(request, 'base/task.html', context)
