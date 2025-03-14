55c35da976ecdc5250b74b8e7a6c6b579717eb4c
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2025-03-14 09:32:55 -0300

    Criei o modelo para a Task

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-updated', '-created']
+    
+    def __str__(self):
+        return self.title[:50]
