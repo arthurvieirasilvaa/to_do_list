e6e3f6e7443b86b625f12c568edaa9634b1dab26
Arthur Vieira Silva <113110861+arthurvieirasilvaa@users.noreply.github.com>
2025-03-19 09:15:07 -0300

    Adicionei widgets no UserForm

--- base/forms.py
+++ base/forms.py
@@ -6,6 +6,20 @@ from .views import Task
 
 
 class UserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs["class"] = "form-input"
+        self.fields["username"].widget.attrs["placeholder"] = "type your username..."
+        
+        self.fields["email"].widget.attrs["class"] = "form-input"
+        self.fields["email"].widget.attrs["placeholder"] = "type your email..."
+        
+        self.fields["password1"].widget.attrs["class"] = "form-input"
+        self.fields["password1"].widget.attrs["placeholder"] = "type your password..."
+                
+        self.fields["password2"].widget.attrs["class"] = "form-input"
+        self.fields["password2"].widget.attrs["placeholder"] = "type your password again..."
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
